bb4e6d3aca995a2c583f9d4cf2dd928213e0fc6f
Hendrik Schreiber <hs@tagtraum.com>
2015-11-16 16:42:12 +0100

    Improved ComplexAudioBuffer test coverage.

--- src/test/java/com/tagtraum/jipes/audio/TestComplexAudioBuffer.java
+++ src/test/java/com/tagtraum/jipes/audio/TestComplexAudioBuffer.java
@@ -15,98 +15,103 @@ import java.util.regex.Pattern;
 import static org.junit.Assert.*;
 
 /**
- * TestRealAudioBuffer.
+ * TestComplexAudioBuffer.
  *
  * @author <a href="mailto:hs@tagtraum.com">Hendrik Schreiber</a>
  */
-public class TestRealAudioBuffer {
+public class TestComplexAudioBuffer {
 
     @Test
     public void testBasics() {
         final int frameNumber = 3;
         final float[] realData = {1, 2, 3, 4};
+        final float[] imaginaryData = {3, 2, 1, 4};
         final AudioFormat audioFormat = new AudioFormat(10, 8, 2, true, false);
-        final RealAudioBuffer buffer = new RealAudioBuffer(frameNumber, realData, audioFormat);
+        final ComplexAudioBuffer buffer = new ComplexAudioBuffer(frameNumber, realData, imaginaryData, audioFormat);
 
         assertEquals(frameNumber, buffer.getFrameNumber());
         assertEquals(realData.length, buffer.getNumberOfSamples());
         assertArrayEquals(realData, buffer.getRealData(), 0.000001f);
-        assertArrayEquals(new float[realData.length], buffer.getImaginaryData(), 0.000001f);
+        assertArrayEquals(imaginaryData, buffer.getImaginaryData(), 0.000001f);
         assertEquals(audioFormat, buffer.getAudioFormat());
         assertEquals((long) (frameNumber * 1000L / audioFormat.getSampleRate()), buffer.getTimestamp());
         assertEquals((long) (frameNumber * 1000L * 1000L / audioFormat.getSampleRate()), buffer.getTimestamp(TimeUnit.MICROSECONDS));
 
-        assertArrayEquals(toMagnitudes(realData, null), buffer.getMagnitudes(), 0.000001f);
-        assertArrayEquals(toPowers(realData, null), buffer.getPowers(), 0.000001f);
-        assertArrayEquals(toMagnitudes(realData, null), buffer.getData(), 0.000001f);
+        assertArrayEquals(toMagnitudes(realData, imaginaryData), buffer.getMagnitudes(), 0.000001f);
+        assertArrayEquals(toPowers(realData, imaginaryData), buffer.getPowers(), 0.000001f);
+        assertArrayEquals(toMagnitudes(realData, imaginaryData), buffer.getData(), 0.000001f);
     }
 
     @Test
     public void testCopyConstructor() {
         final int frameNumber = 3;
         final float[] realData = {1, 2, 3, 4};
+        final float[] imaginaryData = {3, 2, 1, 4};
         final AudioFormat audioFormat = new AudioFormat(10, 8, 2, true, false);
-        final RealAudioBuffer buffer = new RealAudioBuffer(frameNumber, realData, audioFormat);
+        final ComplexAudioBuffer buffer = new ComplexAudioBuffer(frameNumber, realData, imaginaryData, audioFormat);
 
-        final RealAudioBuffer copy = new RealAudioBuffer(buffer);
+        final ComplexAudioBuffer copy = new ComplexAudioBuffer(buffer);
 
         assertEquals(frameNumber, copy.getFrameNumber());
         assertEquals(realData.length, copy.getNumberOfSamples());
         assertArrayEquals(realData, copy.getRealData(), 0.000001f);
         assertNotSame(realData, copy.getRealData());
-        assertArrayEquals(new float[realData.length], copy.getImaginaryData(), 0.000001f);
+        assertArrayEquals(imaginaryData, copy.getImaginaryData(), 0.000001f);
         assertEquals(audioFormat, copy.getAudioFormat());
         assertEquals((long) (frameNumber * 1000L / audioFormat.getSampleRate()), copy.getTimestamp());
         assertEquals((long) (frameNumber * 1000L * 1000L / audioFormat.getSampleRate()), copy.getTimestamp(TimeUnit.MICROSECONDS));
 
-        assertArrayEquals(toMagnitudes(realData, null), copy.getMagnitudes(), 0.000001f);
-        assertArrayEquals(toPowers(realData, null), copy.getPowers(), 0.000001f);
-        assertArrayEquals(toMagnitudes(realData, null), copy.getData(), 0.000001f);
+        assertArrayEquals(toMagnitudes(realData, imaginaryData), copy.getMagnitudes(), 0.000001f);
+        assertArrayEquals(toPowers(realData, imaginaryData), copy.getPowers(), 0.000001f);
+        assertArrayEquals(toMagnitudes(realData, imaginaryData), copy.getData(), 0.000001f);
     }
 
     @Test
     public void testClone() throws CloneNotSupportedException {
         final int frameNumber = 3;
         final float[] realData = {1, 2, 3, 4};
+        final float[] imaginaryData = {3, 2, 1, 4};
         final AudioFormat audioFormat = new AudioFormat(10, 8, 2, true, false);
-        final RealAudioBuffer buffer = new RealAudioBuffer(frameNumber, realData, audioFormat);
+        final ComplexAudioBuffer buffer = new ComplexAudioBuffer(frameNumber, realData, imaginaryData, audioFormat);
 
-        final RealAudioBuffer copy = (RealAudioBuffer)buffer.clone();
+        final ComplexAudioBuffer copy = (ComplexAudioBuffer)buffer.clone();
 
         assertEquals(frameNumber, copy.getFrameNumber());
         assertEquals(realData.length, copy.getNumberOfSamples());
         assertArrayEquals(realData, copy.getRealData(), 0.000001f);
         assertNotSame(realData, copy.getRealData());
-        assertArrayEquals(new float[realData.length], copy.getImaginaryData(), 0.000001f);
+        assertArrayEquals(imaginaryData, copy.getImaginaryData(), 0.000001f);
         assertEquals(audioFormat, copy.getAudioFormat());
         assertEquals((long) (frameNumber * 1000L / audioFormat.getSampleRate()), copy.getTimestamp());
         assertEquals((long) (frameNumber * 1000L * 1000L / audioFormat.getSampleRate()), copy.getTimestamp(TimeUnit.MICROSECONDS));
 
-        assertArrayEquals(toMagnitudes(realData, null), copy.getMagnitudes(), 0.000001f);
-        assertArrayEquals(toPowers(realData, null), copy.getPowers(), 0.000001f);
-        assertArrayEquals(toMagnitudes(realData, null), copy.getData(), 0.000001f);
+        assertArrayEquals(toMagnitudes(realData, imaginaryData), copy.getMagnitudes(), 0.000001f);
+        assertArrayEquals(toPowers(realData, imaginaryData), copy.getPowers(), 0.000001f);
+        assertArrayEquals(toMagnitudes(realData, imaginaryData), copy.getData(), 0.000001f);
     }
 
     @Test
     public void testToString() {
         final int frameNumber = 3;
         final float[] realData = {1, 2, 3, 4};
+        final float[] imaginaryData = {3, 2, 1, 4};
         final AudioFormat audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 10, 16, 2, 16, 10, true);
-        final RealAudioBuffer buffer = new RealAudioBuffer(frameNumber, realData, audioFormat);
-        assertTrue(Pattern.matches("RealAudioBuffer\\{audioFormat=PCM_SIGNED 10\\.0 Hz, 16 bit, stereo, 16 bytes/frame, big-endian, timestamp=300, frameNumber=3, data=\\[.+\\}", buffer.toString()));
+        final ComplexAudioBuffer buffer = new ComplexAudioBuffer(frameNumber, realData, imaginaryData, audioFormat);
+        assertTrue(Pattern.matches("ComplexAudioBuffer\\{audioFormat=PCM_SIGNED 10\\.0 Hz, 16 bit, stereo, 16 bytes/frame, big-endian, timestamp=300, frameNumber=3, realData=\\[.+, imaginaryData=\\[.+\\}", buffer.toString()));
     }
 
     @Test
     public void testEqualsHashCode() {
         final int frameNumber = 3;
         final float[] realData = {1, 2, 3, 4};
+        final float[] imaginaryData = {3, 2, 1, 4};
         final AudioFormat audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 10, 16, 2, 16, 10, true);
-        final RealAudioBuffer buffer0 = new RealAudioBuffer(frameNumber, realData, audioFormat);
-        final RealAudioBuffer buffer1 = new RealAudioBuffer(frameNumber, realData, audioFormat);
-        final RealAudioBuffer buffer2 = new RealAudioBuffer(frameNumber+1, realData, audioFormat);
+        final ComplexAudioBuffer buffer0 = new ComplexAudioBuffer(frameNumber, realData, imaginaryData, audioFormat);
+        final ComplexAudioBuffer buffer1 = new ComplexAudioBuffer(frameNumber, realData, imaginaryData, audioFormat);
+        final ComplexAudioBuffer buffer2 = new ComplexAudioBuffer(frameNumber+1, realData, imaginaryData, audioFormat);
 
-        final RealAudioBuffer buffer3 = new RealAudioBuffer(frameNumber, new float[] {2, 3, 1, 3}, audioFormat);
-        final RealAudioBuffer buffer4 = new RealAudioBuffer(frameNumber, realData, new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 10, 16, 2, 16, 10, false));
+        final ComplexAudioBuffer buffer3 = new ComplexAudioBuffer(frameNumber, new float[] {2, 3, 1, 3}, imaginaryData, audioFormat);
+        final ComplexAudioBuffer buffer4 = new ComplexAudioBuffer(frameNumber, realData, imaginaryData, new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 10, 16, 2, 16, 10, false));
 
         assertEquals(buffer0, buffer1);
         assertEquals(buffer0.hashCode(), buffer1.hashCode());
