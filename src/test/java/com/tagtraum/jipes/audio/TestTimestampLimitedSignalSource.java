40dc709967421d50988204cf1f1cd4d66717af0a
Hendrik Schreiber <hs@tagtraum.com>
2015-11-19 10:07:35 +0100

    Improved test coverage for TimestampLimitedSignalSource.

--- src/test/java/com/tagtraum/jipes/audio/TestTimestampLimitedSignalSource.java
+++ src/test/java/com/tagtraum/jipes/audio/TestTimestampLimitedSignalSource.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import javax.sound.sampled.AudioFormat;
+import java.io.Closeable;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
@@ -29,25 +30,7 @@ public class TestTimestampLimitedSignalSource {
 
     @Before
     public void setup() {
-        this.monoSource = new SignalSource<AudioBuffer>() {
-
-            private int frameNumber = 0;
-
-            public void reset() {
-                frameNumber = 0;
-            }
-
-            public AudioBuffer read() throws IOException {
-                final RealAudioBuffer buffer = new RealAudioBuffer(
-                        frameNumber, new float[]{
-                        10, 10, 10, 10, 10,
-                        10, 10, 10, 10, 10,
-                }, new AudioFormat(10000, 8, 1, true, true)
-                );
-                frameNumber += buffer.getNumberOfSamples();
-                return buffer;
-            }
-        };
+        this.monoSource = new CloseableSignalSource();
     }
 
     @Test
@@ -67,10 +50,50 @@ public class TestTimestampLimitedSignalSource {
     }
 
     @Test
+    public void testReset() throws IOException {
+        final long maxTimestampInMS = 1000l;
+        final TimestampLimitedSignalSource<AudioBuffer> source = new TimestampLimitedSignalSource<AudioBuffer>(monoSource, maxTimestampInMS);
+        assertEquals(maxTimestampInMS, source.getMaxTimestamp(TimeUnit.MILLISECONDS));
+        assertEquals(monoSource, source.getSignalSource());
+
+        source.read();
+        source.read();
+        source.reset();
+        assertEquals(0, source.read().getFrameNumber());
+    }
+
+    @Test
     public void testToString() throws IOException {
         final long maxTimestampInMS = 1000l;
         final TimestampLimitedSignalSource<AudioBuffer> source = new TimestampLimitedSignalSource<AudioBuffer>(monoSource, maxTimestampInMS);
 
         assertTrue(Pattern.matches("TimestampLimitedSignalSource\\{maxTimestamp=1000 milliseconds, signalSource=.+\\}", source.toString()));
+    }
+
+    private static class CloseableSignalSource implements SignalSource<AudioBuffer>, Closeable {
+
+        private int frameNumber = 0;
+        private boolean closed;
+
+        public void reset() {
+            frameNumber = 0;
+        }
+
+        public AudioBuffer read() throws IOException {
+            final RealAudioBuffer buffer = new RealAudioBuffer(
+                    frameNumber, new float[]{
+                    10, 10, 10, 10, 10,
+                    10, 10, 10, 10, 10,
+            }, new AudioFormat(10000, 8, 1, true, true)
+            );
+            frameNumber += buffer.getNumberOfSamples();
+            return buffer;
+        }
+
+        @Override
+        public void close() throws IOException {
+            if (closed) throw new IOException("Already closed");
+            closed = true;
+        }
     }
 }
