9531f827f2862258412ddc15e8ea57b6b1bbebb6
Hendrik Schreiber <hs@tagtraum.com>
2018-01-15 22:09:18 +0100

    Fixed bad javadoc (HTML).

--- src/main/java/com/tagtraum/jipes/math/DistanceFunction.java
+++ src/main/java/com/tagtraum/jipes/math/DistanceFunction.java
@@ -8,10 +8,7 @@ package com.tagtraum.jipes.math;
 
 /**
  * Useful for calculating the distance between two objects of the same type,
- * for example <code>float<code> or <code>double</code> arrays.
- * <p/>
- * Date: Sep 15, 2010
- * Time: 8:58:14 AM
+ * for example <code>float</code> or <code>double</code> arrays.
  *
  * @author <a href="mailto:hs@tagtraum.com">Hendrik Schreiber</a>
  * @see Floats
